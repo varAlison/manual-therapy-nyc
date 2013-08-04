0c9c6d64fce17ac5ee77338bbd2aba526b906591
alison <alisonlin@outlook.com>
2013-08-05 01:02:22 +0200

    Updated header and images

--- headerbottom.php
+++ headerbottom.php
@@ -1,7 +1,7 @@
 		<header id = "custom-header">
-			<div id = "logoTop">Manual Therapy</a></div>
+			<div id = "logoTop"><a href="http://www.manualtherapynyc.com">Manual Therapy</a></div>
 			<div id = "businessInfo">
-				<div id = "address">80 East 11th Street, Suite 606</div>
+				<div id = "address">80 East 11th Street, #606</div>
 				<div id = "logoBottom">New York City</div>
 				<div id = "phone">+1.646.417.1837</div>
 			</div>
