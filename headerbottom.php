a7365ed589dbad3ed2b3c814d28424fb4d7425f5
cw <conway@xerpi.com>
2013-08-05 16:44:40 +0200

    added google tag manager

--- headerbottom.php
+++ headerbottom.php
@@ -1,3 +1,13 @@
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PBB8MH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PBB8MH');</script>
+<!-- End Google Tag Manager -->
+
 		<header id = "custom-header">
 			<div id = "logoTop"><a href="http://www.manualtherapynyc.com">Manual Therapy</a></div>
 			<div id = "businessInfo">
