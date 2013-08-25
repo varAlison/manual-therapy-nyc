bf494fad6730a78e96ed57ac54a5db87c1bb6313
alison <alisonlin@outlook.com>
2013-08-25 14:44:48 -0400

    Updated contact page and styles for contact page
    For design and map placement

--- contact.php
+++ contact.php
@@ -3,8 +3,10 @@
 <?php include 'headerbottom.php'; ?>	
 
 		<section id="contact">
+			<img src="/images/contact.jpg" alt="manual therapy nyc" class="initialImage">
+			<div class="contactContent">
 			<address>
-			<p><img src="/images/contact.jpg" alt="manual therapy nyc" class="initialImage">
+			<p>
 				<span class="contactTitle">Manual Therapy NYC</span></br>
 				St Denis Building</br>
 				80 East 11th Street, PH 606</br>
@@ -15,10 +17,9 @@
 			</address>
 			<h3>Newsletter:</h3>
 			<p>	
-				Send us a note to be added to our newsletter at <span class = "code">&#109;&#111;&#099;&#046;&#099;&#121;&#110;&#121;&#112;&#097;&#114;&#101;&#104;&#116;&#108;&#097;&#117;&#110;&#097;&#109;&#064;&#111;&#102;&#110;&#105;</span>. 
-			</p>	
-			<div id="map">
-				<iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=80+East+11th+street,+New+York,+NY+10003&amp;aq=&amp;sll=40.653555,-73.913269&amp;sspn=1.13561,2.694397&amp;ie=UTF8&amp;hq=&amp;hnear=80+E+11th+St,+New+York,+10003&amp;ll=40.732616,-73.991731&amp;spn=0.008862,0.02105&amp;t=m&amp;z=14&amp;output=embed"></iframe>
+				Send us a note to be added to our newsletter at <span class = "code">&#109;&#111;&#099;&#046;&#099;&#121;&#110;&#121;&#112;&#097;&#114;&#101;&#104;&#116;&#108;&#097;&#117;&#110;&#097;&#109;&#064;&#111;&#102;&#110;&#105;</span>. 	
+				<iframe id="map" width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=80+East+11th+street,+New+York,+NY+10003&amp;aq=&amp;sll=40.653555,-73.913269&amp;sspn=1.13561,2.694397&amp;ie=UTF8&amp;hq=&amp;hnear=80+E+11th+St,+New+York,+10003&amp;ll=40.732616,-73.991731&amp;spn=0.008862,0.02105&amp;t=m&amp;z=14&amp;output=embed"></iframe>
+			</p>
 			</div>
 		</section>			
 
